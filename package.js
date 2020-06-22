f6b01e0e00fb9ec4857ac7bc688c1ffcc53f0692
Jan Küster <jkuester@uni-bremen.de>
2020-06-22 11:53:50 +0200

    published 1.0.0

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Rate-limit methods and publications. Lightweight. Simple.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:leaonline/ratelimit-factory.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
